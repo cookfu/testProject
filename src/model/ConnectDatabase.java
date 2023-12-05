714c71e85afafed0a5e4a012dd6c9b6c308e73a0
cookfu <cookfu_1026@126.com>
2023-12-05 22:57:32 +0800

    testUpdate

--- src/model/ConnectDatabase.java
+++ src/model/ConnectDatabase.java
@@ -6,8 +6,9 @@ import java.sql.DriverManager;
 public class ConnectDatabase {
 	Connection con = null;
 	private static String url = "jdbc:mysql://localhost:3306/curriculum_design";
-    private static String user = "root";          //mysql登录名
-    private static String pass = "";          //mysql登录密码
+    	private static String user = "root";          //mysql登录名
+    	private static String pass = "";          //mysql登录密码
+	int a = 1;
 	public final void connectDatabase() {
 	    try{
 	        con=DriverManager.getConnection(url,user,pass);//连接数据库代码
